ac6037e27b6c27a07230b0326b9366f3e588aa5b
Ante Kresic <ante.kresic@gmail.com>
2016-05-14 17:42:04 +0200

    Added solution to problem 4

---
+++ 1-99/1-10/4.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	i := 1000000
+mainloop:
+	for {
+		i--
+		if !isPalindromeNumber(i) {
+			continue
+		}
+
+		for j := 999; j > 99; j-- {
+			if i%j == 0 && i/j < 1000 {
+				break mainloop
+			}
+		}
+	}
+
+	println(i)
+}
+
+func isPalindromeNumber(number int) bool {
+	original := number
+	reverse := 0
+
+	for number != 0 {
+		reverse = reverse*10 + number%10
+		number /= 10
+	}
+
+	return original == reverse
+}
